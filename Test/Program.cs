a5a8279378558c77f7f669dfa243b0f499854a6b
Alexander <sany199574@gmail.com>
2023-08-20 06:15:01 +0300

    Добавьте файлы проекта.

---
+++ Test/Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+
+
+
+
+
+
+Console.ReadLine();
+
+
+public class WeatherForecast
+{
+    public DateTimeOffset Date { get; set; }
+    public int TemperatureCelsius { get; set; }
+    public string? Summary { get; set; }
+}
