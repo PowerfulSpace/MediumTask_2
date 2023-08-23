f8fe8b8e7747b95c277afd9ce8d3c0bfd465ff0b
Alexander <sany199574@gmail.com>
2023-08-23 07:47:32 +0300

    Работа всех потоков и работа потоков ввода ввывода. Тестовые испытания

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,38 +1,47 @@
 ﻿
 
+Console.WriteLine("Начало работы программы");
+ShowThreadInfo();
+Console.WriteLine("Запускаем Task1 в потоке из пула потоков");
+ThreadPool.QueueUserWorkItem(new WaitCallback(Task1));
+ShowThreadInfo();
+Console.WriteLine("Запускаем Task2 в потоке из пула потоков");
+Thread.Sleep(1000);
+ThreadPool.QueueUserWorkItem(Task2);
+ShowThreadInfo();
+Console.WriteLine("Главный поток.");
+
+Thread.Sleep(1000);
+
+Console.WriteLine("Главный поток завершен.\n");
+
+Console.WriteLine("Task1 и Task2 закончили работу");
+ShowThreadInfo();
+Console.ReadKey();
 
 
-Console.SetWindowSize(80, 40);
 
-MyClass instance = new MyClass();
-
-for (int i = 0; i < 3; i++)
+static void Task1(Object state)
 {
-    new Thread(instance.Method).Start();
+    Thread.CurrentThread.Name = "1";
+    Console.WriteLine("Поток {0}:{1}", Thread.CurrentThread.Name, Thread.CurrentThread.ManagedThreadId);
+    Console.WriteLine();
+    Thread.Sleep(200);
 }
 
-Thread.Sleep(500);
-
-
-
-Console.ReadLine();
-
-class MyClass
+static void Task2(Object state)
 {
-    object block = new object();
+    Thread.CurrentThread.Name = "2";
+    Console.WriteLine("Поток {0}:{1}", Thread.CurrentThread.Name, Thread.CurrentThread.ManagedThreadId);
+    Thread.Sleep(200);
+}
 
-    public void Method()
-    {
-        int hash = Thread.CurrentThread.GetHashCode();
 
-        lock (block)
-        {
-            for (int counter = 0; counter < 10; counter++)
-            {
-                Console.WriteLine("Поток # {0}: шаг {1}", hash, counter);
-                Thread.Sleep(100);
-            }
-            Console.WriteLine(new string('-', 20));
-        }
-    }
+static void ShowThreadInfo()
+{
+    int availableWorkThreads, availableIOThreads, maxWorkThreads, maxIOThreads;
+    ThreadPool.GetAvailableThreads(out availableWorkThreads, out availableIOThreads);
+    ThreadPool.GetMaxThreads(out maxWorkThreads, out maxIOThreads);
+    Console.WriteLine("-------------Доступно рабочих потоков в пуле:{0} из {1}", availableWorkThreads, maxWorkThreads);
+    Console.WriteLine("-------------Доступно потоков ввода-вывода в пуле:{0} из {1}\n", availableIOThreads, maxIOThreads);
 }
