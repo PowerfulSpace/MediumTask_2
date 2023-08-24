8c12575d978ab6e985a73f061645b502b2354040
Alexander <sany199574@gmail.com>
2023-08-24 07:43:06 +0300

    Проверка тестового примера по оптимизации

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,33 +1,27 @@
-﻿class Program
+﻿
+//Создайте приложение, которое может быть запущено только в одном экземпляре (используя именованный Mutex). 
+
+
+public class Program
 {
-    static EventWaitHandle manual = null; // базовый сигнал для сигнальщика
-
-    static void Main()
+    public static bool flag;
+  
+    public static void Main()
     {
-
-        manual = new EventWaitHandle(false, EventResetMode.ManualReset, "GlobalEvent::GUID");// до первой строчки не знаем какой будет сигнальщик. Номер уникальный,какой сигнальщик создавать
-
         Thread thread = new Thread(Function);
-        thread.IsBackground = true;
         thread.Start();
+        Thread.Sleep(1000);
+        flag = true;
 
-        Console.WriteLine("Нажмите любую клавишу для начала работы потока.");
-        Console.ReadKey();
-
-
-        manual.Set();
-
+        Console.WriteLine("-----------");
         Console.ReadKey();
     }
-
+    
     static void Function()
     {
-        manual.WaitOne(); // будет ожидать сигнала
-
-        while (true)
+        while (!flag)
         {
-            Console.WriteLine("Hello world!");
-            Thread.Sleep(300);
-        }
+            Console.WriteLine("Function");
+        } 
     }
 }
