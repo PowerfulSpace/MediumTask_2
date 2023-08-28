0cbb1fb5a44d8f839344f73f5c96ce6ad903dfe2
Alexander <sany199574@gmail.com>
2023-08-28 13:26:03 +0300

    Добавлен пример Как группировать файлы по расширению

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,29 +1,84 @@
-﻿class Program
+﻿class GroupByExtension
 {
-    static void Main()
+    // This query will sort all the files under the specified folder  
+    //  and subfolder into groups keyed by the file extension.  
+    private static void Main()
     {
-        int[] data = new int[10000000];
+        // Take a snapshot of the file system.  
+        string startFolder = @"c:\program files\Microsoft Visual Studio 9.0\Common7";
 
-        for (int i = 0; i < data.Length; i++) data[i] = i;
+        // Used in WriteLine to trim output lines.  
+        int trimLength = startFolder.Length;
 
-        data[1000] = -1;
-        data[14000] = -2;
-        data[15000] = -3;
-        data[676000] = -4;
-        data[8024540] = -5;
-        data[9908000] = -6;
-        data[9908600] = -6;
-        data[9908800] = -6;
-        data[9908400] = -6;
+        // Take a snapshot of the file system.  
+        System.IO.DirectoryInfo dir = new System.IO.DirectoryInfo(startFolder);
 
-        // Запрос PLINQ для поиска отрицательных значений с использованием метода AsOrdered() 
-        // для сохранения порядка в результирующей последовательности.
-        var negatives = data.AsParallel().AsOrdered().
-                            Where(val => val < 0);
+        // This method assumes that the application has discovery permissions  
+        // for all folders under the specified path.  
+        IEnumerable<System.IO.FileInfo> fileList = dir.GetFiles("*.*", System.IO.SearchOption.AllDirectories);
 
-        foreach (var v in negatives)
-            Console.Write(v + " ");
+        // Create the query.  
+        var queryGroupByExt =
+            from file in fileList
+            group file by file.Extension.ToLower() into fileGroup
+            orderby fileGroup.Key
+            select fileGroup;
 
-        Console.ReadKey();
+        // Display one group at a time. If the number of
+        // entries is greater than the number of lines  
+        // in the console window, then page the output.  
+        PageOutput(trimLength, queryGroupByExt);
+    }
+
+    // This method specifically handles group queries of FileInfo objects with string keys.  
+    // It can be modified to work for any long listings of data. Note that explicit typing  
+    // must be used in method signatures. The groupbyExtList parameter is a query that produces  
+    // groups of FileInfo objects with string keys.  
+    private static void PageOutput(int rootLength,
+                                    IEnumerable<System.Linq.IGrouping<string, System.IO.FileInfo>> groupByExtList)
+    {
+        // Flag to break out of paging loop.  
+        bool goAgain = true;
+
+        // "3" = 1 line for extension + 1 for "Press any key" + 1 for input cursor.  
+        int numLines = Console.WindowHeight - 3;
+
+        // Iterate through the outer collection of groups.  
+        foreach (var filegroup in groupByExtList)
+        {
+            // Start a new extension at the top of a page.  
+            int currentLine = 0;
+
+            // Output only as many lines of the current group as will fit in the window.  
+            do
+            {
+                Console.Clear();
+                Console.WriteLine(filegroup.Key == String.Empty ? "[none]" : filegroup.Key);
+
+                // Get 'numLines' number of items starting at number 'currentLine'.  
+                var resultPage = filegroup.Skip(currentLine).Take(numLines);
+
+                //Execute the resultPage query  
+                foreach (var f in resultPage)
+                {
+                    Console.WriteLine("\t{0}", f.FullName.Substring(rootLength));
+                }
+
+                // Increment the line counter.  
+                currentLine += numLines;
+
+                // Give the user a chance to escape.  
+                Console.WriteLine("Press any key to continue or the 'End' key to break...");
+                ConsoleKey key = Console.ReadKey().Key;
+                if (key == ConsoleKey.End)
+                {
+                    goAgain = false;
+                    break;
+                }
+            } while (currentLine < filegroup.Count());
+
+            if (goAgain == false)
+                break;
+        }
     }
 }
