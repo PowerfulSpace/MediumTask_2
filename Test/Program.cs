8095c8f2f67e962ae09dc9a9ea1d502965350b7c
Alexander <sany199574@gmail.com>
2023-08-30 02:50:08 +0300

    Добавлен тестовый пример Улучшенные решения на основе PLINQ: Geonames Helper

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,84 +1,44 @@
-﻿class GroupByExtension
+﻿class Program
 {
-    // This query will sort all the files under the specified folder  
-    //  and subfolder into groups keyed by the file extension.  
-    private static void Main()
+    static void Main(string[] args)
     {
-        // Take a snapshot of the file system.  
-        string startFolder = @"c:\program files\Microsoft Visual Studio 9.0\Common7";
+        const int nameColumn = 1;
+        const int countryColumn = 8;
+        const int elevationColumn = 15;
 
-        // Used in WriteLine to trim output lines.  
-        int trimLength = startFolder.Length;
+        String inFile = "Data/AllCountries.txt";
+        if (args.Length >= 1) inFile = args[0];
 
-        // Take a snapshot of the file system.  
-        System.IO.DirectoryInfo dir = new System.IO.DirectoryInfo(startFolder);
+        int degreeOfParallelism = 1;
+        if (args.Length >= 2) degreeOfParallelism = int.Parse(args[1]);
+        Console.WriteLine("Geographical data file: {0}. 
+    
+          Degree of Parallelism: { 1}.", inFile, degreeOfParallelism);
+  
+    var lines = File.ReadLines(Path.Combine(
+      Environment.CurrentDirectory, inFile));
 
-        // This method assumes that the application has discovery permissions  
-        // for all folders under the specified path.  
-        IEnumerable<System.IO.FileInfo> fileList = dir.GetFiles("*.*", System.IO.SearchOption.AllDirectories);
+        var q = from line in
+          lines.AsParallel().WithDegreeOfParallelism(degreeOfParallelism)
+                let elevation =
+                  Helper.ExtractIntegerField(line, elevationColumn)
+                where elevation > 8000 // elevation in meters
+                orderby elevation descending
+                select new
+                {
+                    elevation = elevation,
+                    thisLine = line
+                };
 
-        // Create the query.  
-        var queryGroupByExt =
-            from file in fileList
-            group file by file.Extension.ToLower() into fileGroup
-            orderby fileGroup.Key
-            select fileGroup;
-
-        // Display one group at a time. If the number of
-        // entries is greater than the number of lines  
-        // in the console window, then page the output.  
-        PageOutput(trimLength, queryGroupByExt);
-    }
-
-    // This method specifically handles group queries of FileInfo objects with string keys.  
-    // It can be modified to work for any long listings of data. Note that explicit typing  
-    // must be used in method signatures. The groupbyExtList parameter is a query that produces  
-    // groups of FileInfo objects with string keys.  
-    private static void PageOutput(int rootLength,
-                                    IEnumerable<System.Linq.IGrouping<string, System.IO.FileInfo>> groupByExtList)
-    {
-        // Flag to break out of paging loop.  
-        bool goAgain = true;
-
-        // "3" = 1 line for extension + 1 for "Press any key" + 1 for input cursor.  
-        int numLines = Console.WindowHeight - 3;
-
-        // Iterate through the outer collection of groups.  
-        foreach (var filegroup in groupByExtList)
+        foreach (var x in q)
         {
-            // Start a new extension at the top of a page.  
-            int currentLine = 0;
-
-            // Output only as many lines of the current group as will fit in the window.  
-            do
+            if (x != null)
             {
-                Console.Clear();
-                Console.WriteLine(filegroup.Key == String.Empty ? "[none]" : filegroup.Key);
-
-                // Get 'numLines' number of items starting at number 'currentLine'.  
-                var resultPage = filegroup.Skip(currentLine).Take(numLines);
-
-                //Execute the resultPage query  
-                foreach (var f in resultPage)
-                {
-                    Console.WriteLine("\t{0}", f.FullName.Substring(rootLength));
-                }
-
-                // Increment the line counter.  
-                currentLine += numLines;
-
-                // Give the user a chance to escape.  
-                Console.WriteLine("Press any key to continue or the 'End' key to break...");
-                ConsoleKey key = Console.ReadKey().Key;
-                if (key == ConsoleKey.End)
-                {
-                    goAgain = false;
-                    break;
-                }
-            } while (currentLine < filegroup.Count());
-
-            if (goAgain == false)
-                break;
+                String[] fields = x.thisLine.Split(new char[] { '\t' });
+                Console.WriteLine("{0} ({1}m) - located in {2}",
+                  fields[nameColumn], fields[elevationColumn],
+                  fields[countryColumn]);
+            }
         }
     }
 }
