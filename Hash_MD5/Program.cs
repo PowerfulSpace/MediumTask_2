46a3a949e788f2fcc2f91c8de1e8cbf5b031ba85
Alexander <sany199574@gmail.com>
2023-09-13 23:12:37 +0300

    Добавлен комментарий

--- Hash_MD5/Program.cs
+++ Hash_MD5/Program.cs
@@ -21,6 +21,7 @@ static string CreateMD5(string input)
     using MD5 hashMD56 = MD5.Create();                      //создаем объект для работы с MD5
 
     var inputBytes = Encoding.ASCII.GetBytes(input);        //преобразуем строку в массив байтов
+    //var inputBytes = Encoding.UTF8.GetBytes(input);         //преобразуем строку в массив байтов
     var hash = hashMD56.ComputeHash(inputBytes);            //получаем хэш в виде массива байтов
     var output = Convert.ToHexString(hash);                 //преобразуем хэш из массива в строку, состоящую из шестнадцатеричных символов в верхнем регистре
     return output;
@@ -32,6 +33,7 @@ static string CreateSHA256(string input)
     using SHA256 hashSHA256 = SHA256.Create();
 
     var inputBytes = Encoding.ASCII.GetBytes(input);
+    //var inputBytes = Encoding.UTF8.GetBytes(input);  //UTF8 лучше использовать для русского текста
     var hash = hashSHA256.ComputeHash(inputBytes);
     var output = Convert.ToHexString(hash);
     return output;
