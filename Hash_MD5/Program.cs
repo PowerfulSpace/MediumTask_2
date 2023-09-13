d2335f1b5fa8e36297ebf55dc4527c291470f4fd
Alexander <sany199574@gmail.com>
2023-09-13 23:05:50 +0300

    Добавлено зеширование SHA256

--- Hash_MD5/Program.cs
+++ Hash_MD5/Program.cs
@@ -4,9 +4,35 @@ using System.Text;
 
 string input = "Hello, World"; //входное сообщение
 string output; //дайджест сообщения (хэш)
-MD5 MD5Hash = MD5.Create(); //создаем объект для работы с MD5
-byte[] inputBytes = Encoding.ASCII.GetBytes(input); //преобразуем строку в массив байтов
-byte[] hash = MD5Hash.ComputeHash(inputBytes); //получаем хэш в виде массива байтов
-output = Convert.ToHexString(hash); //преобразуем хэш из массива в строку, состоящую из шестнадцатеричных символов в верхнем регистре
 
-Console.WriteLine(output);
+Console.WriteLine("MD5");
+output = CreateMD5(input);
+Console.WriteLine(output+"\n");
+
+
+Console.WriteLine("SHA256");
+output = CreateSHA256(input);
+Console.WriteLine(output + "\n");
+
+Console.ReadLine();
+
+static string CreateMD5(string input)
+{
+    using MD5 hashMD56 = MD5.Create();                      //создаем объект для работы с MD5
+
+    var inputBytes = Encoding.ASCII.GetBytes(input);        //преобразуем строку в массив байтов
+    var hash = hashMD56.ComputeHash(inputBytes);            //получаем хэш в виде массива байтов
+    var output = Convert.ToHexString(hash);                 //преобразуем хэш из массива в строку, состоящую из шестнадцатеричных символов в верхнем регистре
+    return output;
+}
+
+
+static string CreateSHA256(string input)
+{
+    using SHA256 hashSHA256 = SHA256.Create();
+
+    var inputBytes = Encoding.ASCII.GetBytes(input);
+    var hash = hashSHA256.ComputeHash(inputBytes);
+    var output = Convert.ToHexString(hash);
+    return output;
+}
