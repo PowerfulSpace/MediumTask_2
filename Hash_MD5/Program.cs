2f025cc9407a00e159c00373583e8a1c86b738f6
Alexander <sany199574@gmail.com>
2023-09-13 22:53:00 +0300

    Добавлен пример хеширования MD5

---
+++ Hash_MD5/Program.cs
@@ -0,0 +1,12 @@
+﻿
+using System.Security.Cryptography;
+using System.Text;
+
+string input = "Hello, World"; //входное сообщение
+string output; //дайджест сообщения (хэш)
+MD5 MD5Hash = MD5.Create(); //создаем объект для работы с MD5
+byte[] inputBytes = Encoding.ASCII.GetBytes(input); //преобразуем строку в массив байтов
+byte[] hash = MD5Hash.ComputeHash(inputBytes); //получаем хэш в виде массива байтов
+output = Convert.ToHexString(hash); //преобразуем хэш из массива в строку, состоящую из шестнадцатеричных символов в верхнем регистре
+
+Console.WriteLine(output);
--- Test/Program.cs
+++ Test/Program.cs
@@ -1,19 +1,3 @@
 ﻿
 
-
-string[] array = new string[1024];
-
-for (int i = 0; i < array.Length; i++)
-{
-    //array[i] = new string('-', (int)Math.Pow(1024, 2));
-    //array[i] = string.Intern(new string('-', (int)Math.Pow(1024, 2)));
-    array[i] = string.Intern(new string('-', (int)Math.Pow(1024, 2)).ToString());
-
-
-
-    Console.WriteLine(GC.GetTotalMemory(false) / (int)Math.Pow(1024, 2) + "MB");
-}
-
-
 Console.ReadLine();
-
