fdfebd0954eccd8a64b1d3957688163627b9216f
Alexander <sany199574@gmail.com>
2023-09-18 00:13:14 +0300

    Добавлен пример на получение запросса от сервера. С выводом информации

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,12 +1,23 @@
-﻿
-using System.Net;
+﻿using System.Net.Http.Json;
 
-var googleEntry = await Dns.GetHostEntryAsync("google.com");
-Console.WriteLine(googleEntry.HostName);
+HttpClient httpClient = new HttpClient();
 
-foreach (var ip in googleEntry.AddressList)
-{
-    Console.WriteLine(ip);
-}
+
+// отправляемый объект 
+Person tom = new Person { Name = "Tom", Age = 38 };
+// создаем JsonContent
+JsonContent content = JsonContent.Create(tom);
+// отправляем запрос
+using var response = await httpClient.PostAsync("https://localhost:7094/create", content);
+Person? person = await response.Content.ReadFromJsonAsync<Person>();
+Console.WriteLine($"{person?.Id} - {person?.Name}");
 
 Console.ReadLine();
+
+
+class Person
+{
+    public string Id { get; set; } = "";
+    public string Name { get; set; } = "";
+    public int Age { get; set; }
+}
