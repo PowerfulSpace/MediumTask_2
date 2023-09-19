021f52cb262484218006052bd3349f4c4250ef0f
Alexander <sany199574@gmail.com>
2023-09-20 00:07:21 +0300

    Добавлена работа с функцией фибоначи

--- Test/Program.cs
+++ Test/Program.cs
@@ -8,6 +8,17 @@ Console.WriteLine($"Факториал числа 4 = {factorial4}");
 Console.WriteLine($"Факториал числа 5 = {factorial5}");
 Console.WriteLine($"Факториал числа 6 = {factorial6}");
 
+Console.WriteLine();
+
+int fib4 = Fibonachi(4);
+int fib5 = Fibonachi(5);
+int fib6 = Fibonachi(6);
+
+Console.WriteLine($"4 число Фибоначчи = {fib4}");
+Console.WriteLine($"5 число Фибоначчи = {fib5}");
+Console.WriteLine($"6 число Фибоначчи = {fib6}");
+
+
 Console.ReadLine();
 
 static int Factorial(int n)
@@ -15,4 +26,10 @@ static int Factorial(int n)
     if (n == 1) return 1;
 
     return n * Factorial(n - 1);
+}
+int Fibonachi(int n)
+{
+    if (n == 0 || n == 1) return n;
+
+    return Fibonachi(n - 1) + Fibonachi(n - 2);
 }
