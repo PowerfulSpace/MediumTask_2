fd0dfc60c890520d248c1c9bb7cc5693e4561484
Alexander <sany199574@gmail.com>
2023-09-19 20:11:23 +0300

    Добавлен тестовый метод, для кодировки.  С использованием UTF-32LE

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,93 +1,14 @@
-﻿using System.Collections;
-using System.Net.Sockets;
+﻿using System.Text;
 
-NodeStack<string> stack = new NodeStack<string>();
-stack.Push("Tom");
-stack.Push("Alice");
-stack.Push("Bob");
-stack.Push("Kate");
 
-foreach (var item in stack)
+
+static string VlcTrackInfo(int id, string name)
 {
-    Console.WriteLine(item);
-}
-Console.WriteLine();
-string header = stack.Peek();
-Console.WriteLine($"Верхушка стека: {header}");
-Console.WriteLine();
+    var _id = id;
 
-header = stack.Pop();
-foreach (var item in stack)
-{
-    Console.WriteLine(item);
-}
-Console.ReadLine();
-
-public class Node<T>
-{
-    public Node(T data)
-    {
-        Data = data;
-    }
-    public T Data { get; set; }
-    public Node<T> Next { get; set; }
-}
-public class NodeStack<T> : IEnumerable<T>
-{
-    Node<T> head;
-    int count;
-
-    public bool IsEmpty
-    {
-        get { return count == 0; }
-    }
-    public int Count
-    {
-        get { return count; }
-    }
-
-    public void Push(T item)
-    {
-        // увеличиваем стек
-        Node<T> node = new Node<T>(item);
-        node.Next = head; // переустанавливаем верхушку стека на новый элемент
-        head = node;
-        count++;
-    }
-    public T Pop()
-    {
-        // если стек пуст, выбрасываем исключение
-        if (IsEmpty)
-            throw new InvalidOperationException("Стек пуст");
-        Node<T> temp = head;
-        head = head.Next; // переустанавливаем верхушку стека на следующий элемент
-        count--;
-        return temp.Data;
-    }
-    public T Peek()
-    {
-        if (IsEmpty)
-            throw new InvalidOperationException("Стек пуст");
-        return head.Data;
-    }
-
-    IEnumerator IEnumerable.GetEnumerator()
-    {
-        return ((IEnumerable)this).GetEnumerator();
-    }
-
-    IEnumerator<T> IEnumerable<T>.GetEnumerator()
-    {
-        Node<T> current = head;
-        while (current != null)
-        {
-            yield return current.Data;
-            current = current.Next;
-        }
-    }
-
-    public System.Collections.IEnumerator GetEnumerator()
-    {
-        throw new NotImplementedException();
-    }
+    Encoding utf8 = Encoding.UTF8;
+    Encoding utf32 = Encoding.GetEncoding("UTF-32LE");
+    byte[] text = Encoding.Convert(utf8, utf32, utf8.GetBytes(name));
+    var _name = utf32.GetString(text);
+    return _name;
 }
