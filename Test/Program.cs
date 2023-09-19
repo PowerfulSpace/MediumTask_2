eb9bf9dc525a967fd33f433685c28554a72bed00
Alexander <sany199574@gmail.com>
2023-09-20 00:02:49 +0300

    Задача с отсортировкой массива

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,19 +1,27 @@
 ﻿
 
-int[] numbers = { -4, -3, -2, -1, 0, 1, 2, 3, 4 };
+int[] nums = { 54, 7, -41, 2, 4, 2, 89, 33, -5, 12 };
 
-int n = numbers.Length; // длина массива
-int k = n / 2;          // середина массива
-int temp;               // вспомогательный элемент для обмена значениями
-for (int i = 0; i < k; i++)
+// сортировка
+int temp;
+for (int i = 0; i < nums.Length - 1; i++)
 {
-    temp = numbers[i];
-    numbers[i] = numbers[n - i - 1];
-    numbers[n - i - 1] = temp;
+    for (int j = i + 1; j < nums.Length; j++)
+    {
+        if (nums[i] > nums[j])
+        {
+            temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+        }
+    }
 }
-foreach (int i in numbers)
+
+// вывод
+Console.WriteLine("Вывод отсортированного массива");
+for (int i = 0; i < nums.Length; i++)
 {
-    Console.Write($"{i} \t");
+    Console.WriteLine(nums[i]);
 }
 
 Console.ReadLine();
