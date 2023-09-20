e7ce2d0bcb409c30f3ace4bb5055cac02827feef
Alexander <sany199574@gmail.com>
2023-09-21 01:52:50 +0300

    Работа с диагностикой

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,12 +1,24 @@
 ﻿
 
-string longMessage = """
-    This is a long message.
-    It has several lines.
-        Some are indented
-                more than others.
-    Some should start at the first column.
-    Some have "quoted text" in them.
-    """;
+using System.Diagnostics;
+
+string sProdName = "Widget";
+int iUnitQty = 100;
+double dUnitCost = 1.03;
+
+Trace.WriteLine("Trace Information-Product Starting ");
+Trace.Indent();
+
+Trace.WriteLine("The product name is " + sProdName);
+Trace.WriteLine("The product name is" + sProdName, "Field");
+Trace.WriteLineIf(iUnitQty > 50, "This message WILL appear");
+Trace.Assert(dUnitCost > 1, "Message will NOT appear");
+
+Trace.Unindent();
+Trace.WriteLine("Trace Information-Product Ending");
+
+Trace.Flush();
 
 Console.ReadLine();
+
+
