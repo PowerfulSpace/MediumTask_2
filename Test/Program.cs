043a3e2e94d70b4f5a8e234367c48bd86fd81844
Alexander <sany199574@gmail.com>
2023-09-22 00:17:36 +0300

    Добавлен тестовый пример жизненный цикл зависимостей
    Transient: при каждом обращении к сервису создается новый объект сервиса
    
    Scoped: создается отдельный контекст или scope, и в рамках этого контекста при всех обращениях у сервису будет использоваться один и тот же объект сервиса.
    
    Singleton: объект сервиса создается при первом обращении к нему, все последующих обращениях используется один и тот же ранее созданный объект сервиса

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,55 +1,35 @@
 ﻿
-
-using System.Diagnostics;
-
-string sProdName = "Widget";
-int iUnitQty = 100;
-double dUnitCost = 1.03;
-Debug.WriteLine("Debug Information-Product Starting ");
-Debug.Indent();
-Debug.WriteLine("The product name is " + sProdName);
-Debug.WriteLine("The available units on hand are" + iUnitQty.ToString());
-Debug.WriteLine("The per unit cost is " + dUnitCost.ToString());
-
-System.Xml.XmlDocument oxml = new System.Xml.XmlDocument();
-Debug.WriteLine(oxml);
-
-Debug.WriteLine("The product name is " + sProdName, "Field");
-Debug.WriteLine("The units on hand are" + iUnitQty, "Field");
-Debug.WriteLine("The per unit cost is" + dUnitCost.ToString(), "Field");
-Debug.WriteLine("Total Cost is " + (iUnitQty * dUnitCost), "Calc");
-
-Debug.WriteLineIf(iUnitQty > 50, "This message WILL appear");
-Debug.WriteLineIf(iUnitQty < 50, "This message will NOT appear");
-
-Debug.Assert(dUnitCost > 1, "Message will NOT appear");
-Debug.Assert(dUnitCost < 1, "Message will appear since dUnitcost < 1 is false");
-
-TextWriterTraceListener tr1 = new TextWriterTraceListener(System.Console.Out);
-Debug.Listeners.Add(tr1);
-
-TextWriterTraceListener tr2 = new TextWriterTraceListener(System.IO.File.CreateText("Output.txt"));
-Debug.Listeners.Add(tr2);
-
-Debug.WriteLine("The product name is " + sProdName);
-Debug.WriteLine("The available units on hand are" + iUnitQty);
-Debug.WriteLine("The per unit cost is " + dUnitCost);
-Debug.Unindent();
-Debug.WriteLine("Debug Information-Product Ending");
-Debug.Flush();
-
-Trace.WriteLine("Trace Information-Product Starting ");
-Trace.Indent();
-
-Trace.WriteLine("The product name is " + sProdName);
-Trace.WriteLine("The product name is" + sProdName, "Field");
-Trace.WriteLineIf(iUnitQty > 50, "This message WILL appear");
-Trace.Assert(dUnitCost > 1, "Message will NOT appear");
-
-Trace.Unindent();
-Trace.WriteLine("Trace Information-Product Ending");
-
-Trace.Flush();
-Console.ReadLine();
+using Microsoft.Extensions.DependencyInjection;
 
 
+
+var services = new ServiceCollection()
+    .AddTransient<ICounter, RandomCounter>();
+
+using ServiceProvider serviceProvider = services.BuildServiceProvider();
+
+PrintCounters();
+PrintCounters();
+
+
+
+using System;
+
+void PrintCounters()
+{
+    var counter1 = serviceProvider.GetService<ICounter>();
+    var counter2 = serviceProvider.GetService<ICounter>();
+    Console.WriteLine($"Counter1: {counter1?.Value}; Counter2: {counter2?.Value}");
+}
+
+public interface ICounter
+{
+    int Value { get; }
+}
+public class RandomCounter : ICounter
+{
+    static Random rnd = new Random();
+    private int _value;
+    public RandomCounter() => _value = rnd.Next(0, 1000000);
+    public int Value => _value;
+}
