c51e1597fadb3d7142cd84c98f9ea27f4beec774
Alexander <sany199574@gmail.com>
2023-09-24 00:15:59 +0300

    Реализовываем мини проект по принипу SOLID Open/Closed Principle
    Используя при этом паттерн стратегия

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,6 +1,51 @@
 ﻿
 
+Cook bob = new Cook("Bob");
+bob.MakeDinner(new PotatoMeal());
+Console.WriteLine();
+bob.MakeDinner(new SaladMeal());
 
 
 Console.ReadLine();
 
+class Cook
+{
+    public string Name { get; set; }
+
+    public Cook(string name)
+    {
+        this.Name = name;
+    }
+
+    public void MakeDinner(IMeal meal)
+    {
+        meal.Make();
+    }
+}
+
+interface IMeal
+{
+    void Make();
+}
+class PotatoMeal : IMeal
+{
+    public void Make()
+    {
+        Console.WriteLine("Чистим картошку");
+        Console.WriteLine("Ставим почищенную картошку на огонь");
+        Console.WriteLine("Сливаем остатки воды, разминаем варенный картофель в пюре");
+        Console.WriteLine("Посыпаем пюре специями и зеленью");
+        Console.WriteLine("Картофельное пюре готово");
+    }
+}
+
+class SaladMeal : IMeal
+{
+    public void Make()
+    {
+        Console.WriteLine("Нарезаем помидоры и огурцы");
+        Console.WriteLine("Посыпаем зеленью, солью и специями");
+        Console.WriteLine("Поливаем подсолнечным маслом");
+        Console.WriteLine("Салат готов");
+    }
+}
