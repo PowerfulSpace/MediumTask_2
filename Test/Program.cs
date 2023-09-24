d53abb9afa24ed8bc7f68f06c656aba47186c9b1
Alexander <sany199574@gmail.com>
2023-09-25 00:05:25 +0300

    Изменение работы конструкций swich для версия языка C# 11

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,3 +1,17 @@
 ﻿
 
+var numbers = new int[] { 10, 20, 30 };
+Console.WriteLine(GetSourceLabel(numbers));  // output: 1
+
+var letters = new List<char> { 'a', 'b', 'c', 'd' };
+Console.WriteLine(GetSourceLabel(letters));  // output: 2
+
+
 Console.ReadLine();
+
+static int GetSourceLabel<T>(IEnumerable<T> source) => source switch
+{
+    Array array => 1,
+    ICollection<T> collection => 2,
+    _ => 3,
+};
