d51514300fd6ec558bf3b5213aa818633347c2fc
Alexander <sany199574@gmail.com>
2023-09-29 06:06:39 +0300

    Добавлен тестовй пример с шщаблоном var

--- Test_2/Program.cs
+++ Test_2/Program.cs
@@ -1,23 +1,22 @@
 ﻿
 
 
+
+
 Console.ReadLine();
 
 
-static string Classify(Point point) => point switch
+
+static bool IsAcceptable(int id, int absLimit) =>
+    SimulateDataFetch(id) is var results
+    && results.Min() >= -absLimit
+    && results.Max() <= absLimit;
+
+static int[] SimulateDataFetch(int id)
 {
-    (0, 0) => "Origin",
-    (1, 0) => "positive X basis end",
-    (0, 1) => "positive Y basis end",
-    _ => "Just a point",
-};
-
-public readonly struct Point
-{
-    public int X { get; }
-    public int Y { get; }
-
-    public Point(int x, int y) => (X, Y) = (x, y);
-
-    public void Deconstruct(out int x, out int y) => (x, y) = (X, Y);
+    var rand = new Random();
+    return Enumerable
+               .Range(start: 0, count: 5)
+               .Select(s => rand.Next(minValue: -10, maxValue: 11))
+               .ToArray();
 }
--- Working_Sample/Program.cs
+++ Working_Sample/Program.cs
@@ -7,6 +7,15 @@ Console.WriteLine(TakeFive(new[] { '1', '2', '3', '4', '5', '6', '7' }));  // ou
 Console.WriteLine(TakeFive(new[] { 'a', 'b', 'c' }));  // output: abc
 
 
+Console.WriteLine();
+
+var numbers = new List<int> { 1, 2, 3 };
+if (SumAndCount(numbers) is (Sum: var sum, Count: > 0))
+{
+    Console.WriteLine($"Sum of [{string.Join(" ", numbers)}] is {sum}");  // output: Sum of [1 2 3] is 6
+}
+
+
 
 Console.ReadLine();
 
@@ -23,3 +32,17 @@ static string TakeFive(object input) => input switch
     null => throw new ArgumentNullException(nameof(input)),
     _ => throw new ArgumentException("Not supported input type."),
 };
+
+//Позиционный шаблон
+static (double Sum, int Count) SumAndCount(IEnumerable<int> numbers)
+{
+    int sum = 0;
+    int count = 0;
+    foreach (int number in numbers)
+    {
+        sum += number;
+        count++;
+    }
+    return (sum, count);
+}
+
