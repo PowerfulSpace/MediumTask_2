70c76fa6cc55e48ce1bd07366af37e4a86a12cd6
Alexander <sany199574@gmail.com>
2023-09-29 05:28:23 +0300

    Добавлен проект, плучение каллендарного сезона

---
+++ CalendarSeason/Program.cs
@@ -0,0 +1,16 @@
+﻿
+Console.WriteLine(GetCalendarSeason(new DateTime(2021, 3, 14)));  // output: spring
+Console.WriteLine(GetCalendarSeason(new DateTime(2021, 7, 19)));  // output: summer
+Console.WriteLine(GetCalendarSeason(new DateTime(2021, 2, 17)));  // output: winter
+
+
+Console.ReadLine();
+
+static string GetCalendarSeason(DateTime date) => date.Month switch
+{
+    >= 3 and < 6 => "spring",
+    >= 6 and < 9 => "summer",
+    >= 9 and < 12 => "autumn",
+    12 or (>= 1 and < 3) => "winter",
+    _ => throw new ArgumentOutOfRangeException(nameof(date), $"Date with unexpected month: {date.Month}."),
+};
--- Test_2/Program.cs
+++ Test_2/Program.cs
@@ -1,15 +1,3 @@
 ﻿
 
-Console.WriteLine(Classify(13));  // output: Too high
-Console.WriteLine(Classify(double.NaN));  // output: Unknown
-Console.WriteLine(Classify(2.4));  // output: Acceptable
-
 Console.ReadLine();
-
-static string Classify(double measurement) => measurement switch
-{
-    < -4.0 => "Too low",
-    > 10.0 => "Too high",
-    double.NaN => "Unknown",
-    _ => "Acceptable",
-};
