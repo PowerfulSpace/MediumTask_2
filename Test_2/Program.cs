c01cf6e5695ad8f4306838f7615371f70017cb33
Alexander <sany199574@gmail.com>
2023-09-30 04:33:11 +0300

    Дабавлена для примера потокобезопасная коллекция ConcurrentQueue

--- Test_2/Program.cs
+++ Test_2/Program.cs
@@ -1,22 +1,36 @@
 ﻿
 
 
+using System.Collections.Concurrent;
 
+ConcurrentQueue<int> cq = new ConcurrentQueue<int>();
 
+for (int i = 0; i < 10000; i++)
+{
+    cq.Enqueue(i);
+}
+
+int result;
+if (!cq.TryPeek(out result))
+{
+    Console.WriteLine("CQ: TryPeek failed when it should have succeeded");
+}
+else if (result != 0)
+{
+    Console.WriteLine("CQ: Expected TryPeek result of 0, got {0}", result);
+}
+
+int outerSum = 0;
+Action action = () =>
+{
+    int localSum = 0;
+    int localValue;
+    while (cq.TryDequeue(out localValue)) localSum += localValue;
+    Interlocked.Add(ref outerSum, localSum);
+};
+
+Parallel.Invoke(action, action, action, action);
+
+Console.WriteLine("outerSum = {0}, should be 49995000", outerSum);
 Console.ReadLine();
 
-
-
-static bool IsAcceptable(int id, int absLimit) =>
-    SimulateDataFetch(id) is var results
-    && results.Min() >= -absLimit
-    && results.Max() <= absLimit;
-
-static int[] SimulateDataFetch(int id)
-{
-    var rand = new Random();
-    return Enumerable
-               .Range(start: 0, count: 5)
-               .Select(s => rand.Next(minValue: -10, maxValue: 11))
-               .ToArray();
-}
