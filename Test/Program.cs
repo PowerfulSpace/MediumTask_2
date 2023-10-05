bdc6529e689d751ee52a517ca52ae59f2cf479fb
Alexander <sany199574@gmail.com>
2023-10-05 10:41:20 +0300

    Добавлен тестовый пример FormattingCode ParsingCode

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,24 +1,40 @@
-﻿
+﻿ShowFormattingCode();
+  
 
-// отправитель - устанавливаем адрес и отображаемое в письме имя
-using System.Net.Mail;
-using System.Net;
+Console.WriteLine("---");
 
-MailAddress from = new MailAddress("somemail@gmail.com", "Tom");
-// кому отправляем
-MailAddress to = new MailAddress("somemail@yandex.ru");
-// создаем объект сообщения
-MailMessage m = new MailMessage(from, to);
-// тема письма
-m.Subject = "Тест";
-// текст письма
-m.Body = "<h2>Письмо-тест работы smtp-клиента</h2>";
-// письмо представляет код html
-m.IsBodyHtml = true;
-// адрес smtp-сервера и порт, с которого будем отправлять письмо
-SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587);
-// логин и пароль
-smtp.Credentials = new NetworkCredential("somemail@gmail.com", "mypassword");
-smtp.EnableSsl = true;
-smtp.Send(m);
-Console.Read();
+ShowParsingCode();
+
+
+void ShowFormattingCode()
+{
+    TimeSpan interval = new TimeSpan(12, 30, 45);
+    string output;
+    try
+    {
+        output = String.Format("{0:r}", interval);
+    }
+    catch (FormatException)
+    {
+        output = "Invalid Format";
+    }
+    Console.WriteLine(output);
+}
+
+void ShowParsingCode()
+{
+    string value = "000000006";
+    try
+    {
+        TimeSpan interval = TimeSpan.Parse(value);
+        Console.WriteLine("{0} --> {1}", value, interval);
+    }
+    catch (FormatException)
+    {
+        Console.WriteLine("{0}: Bad Format", value);
+    }
+    catch (OverflowException)
+    {
+        Console.WriteLine("{0}: Overflow", value);
+    }
+}
