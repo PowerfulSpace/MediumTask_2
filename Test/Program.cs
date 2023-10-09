29aba8953ce68a17228516d02cb965f874eef80e
Alexander <sany199574@gmail.com>
2023-10-10 00:34:18 +0300

    Добавлен пример, показывающий работу атрибута ObsoleteAttribute

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,86 +1,29 @@
-﻿class Point
+﻿public class Example
 {
-    protected int x, y;
+    // Mark OldProperty As Obsolete.
+    [ObsoleteAttribute("This property is obsolete. Use NewProperty instead.", false)]
+    public static string OldProperty
+    { get { return "The old property value."; } }
 
-    public Point() : this(0, 0)
-    { }
+    public static string NewProperty
+    { get { return "The new property value."; } }
 
-    public Point(int x, int y)
+    // Mark CallOldMethod As Obsolete.
+    [ObsoleteAttribute("This method is obsolete. Call CallNewMethod instead.", true)]
+    public static string CallOldMethod()
     {
-        this.x = x;
-        this.y = y;
+        return "You have called CallOldMethod.";
     }
 
-    public override bool Equals(Object obj)
+    public static string CallNewMethod()
     {
-        //Check for null and compare run-time types.
-        if ((obj == null) || !this.GetType().Equals(obj.GetType()))
-        {
-            return false;
-        }
-        else
-        {
-            Point p = (Point)obj;
-            return (x == p.x) && (y == p.y);
-        }
+        return "You have called CallNewMethod.";
     }
 
-    public override int GetHashCode()
-    {
-        return (x << 2) ^ y;
-    }
-
-    public override string ToString()
-    {
-        return String.Format("Point({0}, {1})", x, y);
-    }
-}
-
-sealed class Point3D : Point
-{
-    int z;
-
-    public Point3D(int x, int y, int z) : base(x, y)
-    {
-        this.z = z;
-    }
-
-    public override bool Equals(Object obj)
-    {
-        Point3D pt3 = obj as Point3D;
-        if (pt3 == null)
-            return false;
-        else
-            return base.Equals((Point)obj) && z == pt3.z;
-    }
-
-    public override int GetHashCode()
-    {
-        return (base.GetHashCode() << 2) ^ z;
-    }
-
-    public override String ToString()
-    {
-        return String.Format("Point({0}, {1}, {2})", x, y, z);
-    }
-}
-
-class Example
-{
     public static void Main()
     {
-        Point point2D = new Point(5, 5);
-        Point3D point3Da = new Point3D(5, 5, 2);
-        Point3D point3Db = new Point3D(5, 5, 2);
-        Point3D point3Dc = new Point3D(5, 5, -1);
-
-        Console.WriteLine("{0} = {1}: {2}",
-                          point2D, point3Da, point2D.Equals(point3Da));
-        Console.WriteLine("{0} = {1}: {2}",
-                          point2D, point3Db, point2D.Equals(point3Db));
-        Console.WriteLine("{0} = {1}: {2}",
-                          point3Da, point3Db, point3Da.Equals(point3Db));
-        Console.WriteLine("{0} = {1}: {2}",
-                          point3Da, point3Dc, point3Da.Equals(point3Dc));
+        Console.WriteLine(OldProperty);
+        Console.WriteLine();
+        Console.WriteLine(CallOldMethod());
     }
 }
