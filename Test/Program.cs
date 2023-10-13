1b3fa53d7ce676296108ac0e4e7480bc56434f49
Alexander <sany199574@gmail.com>
2023-10-14 00:21:42 +0300

    Добавлен тестовф пример Алгоритм дейкстры

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,41 +1,214 @@
-﻿// Normal pointer to an object.
-int[] a = new int[5] { 10, 20, 30, 40, 50 };
-// Must be in unsafe code to use interior pointers.
-unsafe
+﻿
+
+
+var g = new Graph();
+
+//добавление вершин
+g.AddVertex("A");
+g.AddVertex("B");
+g.AddVertex("C");
+g.AddVertex("D");
+g.AddVertex("E");
+g.AddVertex("F");
+g.AddVertex("G");
+
+//добавление ребер
+g.AddEdge("A", "B", 22);
+g.AddEdge("A", "C", 33);
+g.AddEdge("A", "D", 61);
+g.AddEdge("B", "C", 47);
+g.AddEdge("B", "E", 93);
+g.AddEdge("C", "D", 11);
+g.AddEdge("C", "E", 79);
+g.AddEdge("C", "F", 63);
+g.AddEdge("D", "F", 41);
+g.AddEdge("E", "F", 17);
+g.AddEdge("E", "G", 58);
+g.AddEdge("F", "G", 84);
+
+var dijkstra = new Dijkstra(g);
+var path = dijkstra.FindShortestPath("A", "G");
+Console.WriteLine(path);
+Console.ReadLine();
+
+
+
+
+/// <summary>
+/// Информация о вершине
+/// </summary>
+public class GraphVertexInfo
 {
-    // Must pin object on heap so that it doesn't move while using interior pointers.
-    fixed (int* p = &a[0])
+    /// <summary>
+    /// Вершина
+    /// </summary>
+    public GraphVertex Vertex { get; set; }
+
+    /// <summary>
+    /// Не посещенная вершина
+    /// </summary>
+    public bool IsUnvisited { get; set; }
+
+    /// <summary>
+    /// Сумма весов ребер
+    /// </summary>
+    public int EdgesWeightSum { get; set; }
+
+    /// <summary>
+    /// Предыдущая вершина
+    /// </summary>
+    public GraphVertex PreviousVertex { get; set; }
+
+    /// <summary>
+    /// Конструктор
+    /// </summary>
+    /// <param name="vertex">Вершина</param>
+    public GraphVertexInfo(GraphVertex vertex)
     {
-        // p is pinned as well as object, so create another pointer to show incrementing it.
-        int* p2 = p;
-        Console.WriteLine(*p2);
-        // Incrementing p2 bumps the pointer by four bytes due to its type ...
-        p2 += 1;
-        Console.WriteLine(*p2);
-        p2 += 1;
-        Console.WriteLine(*p2);
-        Console.WriteLine("--------");
-        Console.WriteLine(*p);
-        // Dereferencing p and incrementing changes the value of a[0] ...
-        *p += 1;
-        Console.WriteLine(*p);
-        *p += 1;
-        Console.WriteLine(*p);
+        Vertex = vertex;
+        IsUnvisited = true;
+        EdgesWeightSum = int.MaxValue;
+        PreviousVertex = null;
     }
 }
+/// <summary>
+/// Алгоритм Дейкстры
+/// </summary>
+public class Dijkstra
+{
+    Graph graph;
 
-Console.WriteLine("--------");
-Console.WriteLine(a[0]);
+    List<GraphVertexInfo> infos;
 
-/*
-Output:
-10
-20
-30
---------
-10
-11
-12
---------
-12
-*/
+    /// <summary>
+    /// Конструктор
+    /// </summary>
+    /// <param name="graph">Граф</param>
+    public Dijkstra(Graph graph)
+    {
+        this.graph = graph;
+    }
+
+    /// <summary>
+    /// Инициализация информации
+    /// </summary>
+    void InitInfo()
+    {
+        infos = new List<GraphVertexInfo>();
+        foreach (var v in graph.Vertices)
+        {
+            infos.Add(new GraphVertexInfo(v));
+        }
+    }
+
+    /// <summary>
+    /// Получение информации о вершине графа
+    /// </summary>
+    /// <param name="v">Вершина</param>
+    /// <returns>Информация о вершине</returns>
+    GraphVertexInfo GetVertexInfo(GraphVertex v)
+    {
+        foreach (var i in infos)
+        {
+            if (i.Vertex.Equals(v))
+            {
+                return i;
+            }
+        }
+
+        return null;
+    }
+
+    /// <summary>
+    /// Поиск непосещенной вершины с минимальным значением суммы
+    /// </summary>
+    /// <returns>Информация о вершине</returns>
+    public GraphVertexInfo FindUnvisitedVertexWithMinSum()
+    {
+        var minValue = int.MaxValue;
+        GraphVertexInfo minVertexInfo = null;
+        foreach (var i in infos)
+        {
+            if (i.IsUnvisited && i.EdgesWeightSum < minValue)
+            {
+                minVertexInfo = i;
+                minValue = i.EdgesWeightSum;
+            }
+        }
+
+        return minVertexInfo;
+    }
+
+    /// <summary>
+    /// Поиск кратчайшего пути по названиям вершин
+    /// </summary>
+    /// <param name="startName">Название стартовой вершины</param>
+    /// <param name="finishName">Название финишной вершины</param>
+    /// <returns>Кратчайший путь</returns>
+    public string FindShortestPath(string startName, string finishName)
+    {
+        return FindShortestPath(graph.FindVertex(startName), graph.FindVertex(finishName));
+    }
+
+    /// <summary>
+    /// Поиск кратчайшего пути по вершинам
+    /// </summary>
+    /// <param name="startVertex">Стартовая вершина</param>
+    /// <param name="finishVertex">Финишная вершина</param>
+    /// <returns>Кратчайший путь</returns>
+    public string FindShortestPath(GraphVertex startVertex, GraphVertex finishVertex)
+    {
+        InitInfo();
+        var first = GetVertexInfo(startVertex);
+        first.EdgesWeightSum = 0;
+        while (true)
+        {
+            var current = FindUnvisitedVertexWithMinSum();
+            if (current == null)
+            {
+                break;
+            }
+
+            SetSumToNextVertex(current);
+        }
+
+        return GetPath(startVertex, finishVertex);
+    }
+
+    /// <summary>
+    /// Вычисление суммы весов ребер для следующей вершины
+    /// </summary>
+    /// <param name="info">Информация о текущей вершине</param>
+    void SetSumToNextVertex(GraphVertexInfo info)
+    {
+        info.IsUnvisited = false;
+        foreach (var e in info.Vertex.Edges)
+        {
+            var nextInfo = GetVertexInfo(e.ConnectedVertex);
+            var sum = info.EdgesWeightSum + e.EdgeWeight;
+            if (sum < nextInfo.EdgesWeightSum)
+            {
+                nextInfo.EdgesWeightSum = sum;
+                nextInfo.PreviousVertex = info.Vertex;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Формирование пути
+    /// </summary>
+    /// <param name="startVertex">Начальная вершина</param>
+    /// <param name="endVertex">Конечная вершина</param>
+    /// <returns>Путь</returns>
+    string GetPath(GraphVertex startVertex, GraphVertex endVertex)
+    {
+        var path = endVertex.ToString();
+        while (startVertex != endVertex)
+        {
+            endVertex = GetVertexInfo(endVertex).PreviousVertex;
+            path = endVertex.ToString() + path;
+        }
+
+        return path;
+    }
+}
