735f2b1ed809d404a851bcff5568a43b6664bcdc
Alexander <sany199574@gmail.com>
2023-10-16 00:06:27 +0300

    Исправлено решшение задания. Приведено в подобающий вид

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,30 +1,32 @@
 ﻿//Дана целочисленная последовательность. Извлечь из нее все нечетные числа,
 //сохранив их исходный порядок следования и удалив все вхождения повторяющихся элементов, кроме первых.
 
-int[] array = new int[] { 8, 1, 4, 7, 9, 2, 1, 8, 2, 5, 1, 9, 6, 3, 10, 5 };
+List<int> _dataTask1 = new List<int> { 3, 13, 12, 157, 14, 77, 75, 13, 200, 156, 14, 21 };
+List<int> result;
 
+Print(_dataTask1);
 
-var q = array.Where(x => x % 2 != 0).Distinct();
+result = Task1(_dataTask1);
 
-var a = from x in array
-        where x % 2 != 0
-        select x;
-
-var b = a.Intersect(array);
-
-
-foreach (var x in q)
-{
-    Console.Write(x + " ");
-}
-
+Print(result);
 
 
 Console.ReadLine();
 
-List<int> _dataTask1 = new List<int> { 3, 13, 12, 157, 14, 77, 75, 13, 200, 156, 14, 21 };
+
 
 static List<int> Task1(List<int> data) =>
     data.Where(x => x % 2 == 1)
         .Distinct()
         .ToList();
+
+
+
+static void Print(List<int> data)
+{
+    foreach (var item in data)
+    {
+        Console.Write(item + " ");
+    }
+    Console.WriteLine();
+}
