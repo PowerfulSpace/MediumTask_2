c84f6e966e1d7d34fd3ec390d009438229f94fcb
Alexander <sany199574@gmail.com>
2023-10-15 10:51:06 +0300

    Добавлена решённая задача по LINQ

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,14 +1,30 @@
-﻿public abstract class Vehicle { }
-public class Car : Vehicle { }
-public class Truck : Vehicle { }
+﻿//Дана целочисленная последовательность. Извлечь из нее все нечетные числа,
+//сохранив их исходный порядок следования и удалив все вхождения повторяющихся элементов, кроме первых.
 
-public static class TollCalculator
+int[] array = new int[] { 8, 1, 4, 7, 9, 2, 1, 8, 2, 5, 1, 9, 6, 3, 10, 5 };
+
+
+var q = array.Where(x => x % 2 != 0).Distinct();
+
+var a = from x in array
+        where x % 2 != 0
+        select x;
+
+var b = a.Intersect(array);
+
+
+foreach (var x in q)
 {
-    public static decimal CalculateToll(this Vehicle vehicle) => vehicle switch
-    {
-        Car _ => 2.00m,
-        Truck _ => 7.50m,
-        null => throw new ArgumentNullException(nameof(vehicle)),
-        _ => throw new ArgumentException("Unknown type of a vehicle", nameof(vehicle)),
-    };
+    Console.Write(x + " ");
 }
+
+
+
+Console.ReadLine();
+
+List<int> _dataTask1 = new List<int> { 3, 13, 12, 157, 14, 77, 75, 13, 200, 156, 14, 21 };
+
+static List<int> Task1(List<int> data) =>
+    data.Where(x => x % 2 == 1)
+        .Distinct()
+        .ToList();
