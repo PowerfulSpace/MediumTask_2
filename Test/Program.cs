53f815e51575bc3ed28ecc12124e3b65bc151b3b
Alexander <sany199574@gmail.com>
2023-10-19 11:05:36 +0300

    Доабвлен пример по решению задания по LINQ

--- Test/Program.cs
+++ Test/Program.cs
@@ -2,6 +2,7 @@
 //сохранив их исходный порядок следования и удалив все вхождения повторяющихся элементов, кроме первых.
 
 List<int> _dataTask1 = new List<int> { 3, 13, 12, 157, 14, 77, 75, 13, 200, 156, 14, 21 };
+List<int> _dataTask2 = new List<int> { -12, -33, 134, 12, 1, -3, 99, 199, 5, 17, -121, 10 };
 List<int> result;
 
 Print(_dataTask1);
@@ -18,6 +19,14 @@ Console.ReadLine();
 static List<int> Task1(List<int> data) =>
     data.Where(x => x % 2 == 1)
         .Distinct()
+        .ToList();
+
+
+
+
+static List<int> Task2(List<int> data) =>
+    data.Where(x => x > 0 && x.ToString().Length == 2)
+        .OrderBy(x => x)
         .ToList();
 
 
--- Test_2/Program.cs
+++ Test_2/Program.cs
@@ -1,42 +1,58 @@
-﻿using System.Reflection;
+﻿
 
-Type myType = typeof(Person);
+using System.Linq;
+using System.Threading.Tasks;
 
-Console.WriteLine("Поля:");
-foreach (FieldInfo field in myType.GetFields(
-    BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Static))
+//List<int> _dataTask1 = new List<int> { 3, 13, 12, 157, 14, 77, 75, 13, 200, 156, 14, 21 };
+List<int> _dataTask1 = new List<int> { 3, 13, -2, 12, 157, 14, -55, 77, 75, 13, -344, 200, 156, 14, 21, 9, 6, 5 };
+List<int> result;
+
+Print(_dataTask1);
+
+Console.WriteLine("\nЗадача_1");
+result = Task_1(_dataTask1);
+Print(result);
+
+Console.WriteLine("\nЗадача_2");
+result = Task_2(_dataTask1);
+Print(result);
+
+
+Console.ReadLine();
+
+
+//  1.    Дана целочисленная последовательность. Извлечь из нее все нечетные числа,
+//сохранив их исходный порядок следования и удалив все вхождения повторяющихся элементов, кроме первых.
+
+static List<int> Task_1(List<int> items) => items
+    .Where(x => x % 2 != 0)
+    .Distinct()
+    .ToList();
+
+
+
+//  2.  Дана целочисленная последовательность. Извлечь из нее все
+//положительные двузначные числа, отсортировав их по возрастанию.
+
+static List<int> Task_2(List<int> items)
 {
-    string modificator = "";
-
-    // получаем модификатор доступа
-    if (field.IsPublic)
-        modificator += "public ";
-    else if (field.IsPrivate)
-        modificator += "private ";
-    else if (field.IsAssembly)
-        modificator += "internal ";
-    else if (field.IsFamily)
-        modificator += "protected ";
-    else if (field.IsFamilyAndAssembly)
-        modificator += "private protected ";
-    else if (field.IsFamilyOrAssembly)
-        modificator += "protected internal ";
-
-    // если поле статическое
-    if (field.IsStatic) modificator += "static ";
-
-    Console.WriteLine($"{modificator}{field.FieldType.Name} {field.Name}");
+    return items
+        .Where(x => x > 0 && x.ToString().Length == 2)
+        .OrderBy(y => y)
+        .ToList();
 }
 
-class Person
+//  3.  Дана строковая последовательность. Строки последовательности содержат только
+//заглавные буквы латинского алфавита. Отсортировать последовательность
+//по возрастанию длин строк, а строки одинаковой длины – по убыванию.
+
+
+
+static void Print(List<int> items)
 {
-    static int minAge = 0;
-    string name;
-    int age;
-    public Person(string name, int age)
+    foreach (var item in items)
     {
-        this.name = name;
-        this.age = age;
+        Console.Write(item + " ");
     }
-    public void Print() => Console.WriteLine($"{name} - {age}");
+    Console.WriteLine();
 }
