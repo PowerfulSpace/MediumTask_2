a19448e0ea63f1b8fc0b9ae3e4f4fb2ebfae530f
Alexander <sany199574@gmail.com>
2023-10-19 11:58:40 +0300

    ВЫполнил 4 задание

--- Test/Program.cs
+++ Test/Program.cs
@@ -3,13 +3,15 @@
 
 List<int> _dataTask1 = new List<int> { 3, 13, 12, 157, 14, 77, 75, 13, 200, 156, 14, 21 };
 List<int> _dataTask2 = new List<int> { -12, -33, 134, 12, 1, -3, 99, 199, 5, 17, -121, 10 };
+List<string> _dataTask3 = new List<string> { "ADJSLFKHJL", "FAJL", "ADJPLFKHJL", "FATL", "FD", "FAFADFSFDAFR", "FY" };
+List<string> _dataTask4 = new List<string> { "asdf", "Rew", "trafa", "nadf", "Slos", "At", "new" };
+
 List<int> result;
 
-Print(_dataTask1);
-
-result = Task1(_dataTask1);
-
-Print(result);
+Print(Task1(_dataTask1));
+Print(Task2(_dataTask2));
+Print(Task3(_dataTask3));
+Print(Task4(_dataTask4));
 
 
 Console.ReadLine();
@@ -21,17 +23,27 @@ static List<int> Task1(List<int> data) =>
         .Distinct()
         .ToList();
 
-
-
-
 static List<int> Task2(List<int> data) =>
     data.Where(x => x > 0 && x.ToString().Length == 2)
         .OrderBy(x => x)
         .ToList();
 
 
+static List<string> Task3(List<string> data) =>
+    data.OrderBy(x => x.Length)
+        .ThenBy(x => x)
+        .ToList();
 
-static void Print(List<int> data)
+
+
+static List<char> Task4(List<string> data) =>
+    data.Select(x => x[0])
+    .Reverse()
+    .ToList();
+
+
+
+static void Print<T>(List<T> data)
 {
     foreach (var item in data)
     {
--- Test_2/Program.cs
+++ Test_2/Program.cs
@@ -3,24 +3,36 @@
 using System.Linq;
 using System.Threading.Tasks;
 
-//List<int> _dataTask1 = new List<int> { 3, 13, 12, 157, 14, 77, 75, 13, 200, 156, 14, 21 };
-List<int> _dataTask1 = new List<int> { 3, 13, -2, 12, 157, 14, -55, 77, 75, 13, -344, 200, 156, 14, 21, 9, 6, 5 };
-List<int> result;
+List<int> _dataTask1 = new List<int> { 3, 13, 12, 157, 14, 77, 75, 13, 200, 156, 14, 21 };
+List<int> _dataTask2 = new List<int> { 3, 13, -2, 12, 157, 14, -55, 77, 75, 13, -344, 200, 156, 14, 21, 9, 6, 5 };
+List<string> _dataTask3 = new List<string> { "ADJSLFKHJL", "FAJL", "ADJPLFKHJL", "FATL", "FD", "FAFADFSFDAFR", "FY" };
+List<string> _dataTask4 = new List<string> { "asdf", "Rew", "trafa", "nadf", "Slos", "At", "new" };
+List<int> _dataTask5 = new List<int> { -12, -33, 134, 12, 1, -3, 99, 199, 5, 17, -121, 10 };
 
-Print(_dataTask1);
-
+#region Task_1
 Console.WriteLine("\nЗадача_1");
-result = Task_1(_dataTask1);
-Print(result);
-
+Print(Task_1(_dataTask1));
+#endregion
+#region Task_2
 Console.WriteLine("\nЗадача_2");
-result = Task_2(_dataTask1);
-Print(result);
-
+Print(Task_2(_dataTask2));
+#endregion
+#region Task_3
+Console.WriteLine("\nЗадача_3");
+Print(Task_3(_dataTask3));
+#endregion
+#region Task_4
+Console.WriteLine("\nЗадача_4");
+Print(Task_4(_dataTask4));
+#endregion
+#region Task_5
+Console.WriteLine("\nЗадача_5");
+Print(Task_5(_dataTask5));
+#endregion
 
 Console.ReadLine();
 
-
+#region Task_1
 //  1.    Дана целочисленная последовательность. Извлечь из нее все нечетные числа,
 //сохранив их исходный порядок следования и удалив все вхождения повторяющихся элементов, кроме первых.
 
@@ -28,27 +40,50 @@ static List<int> Task_1(List<int> items) => items
     .Where(x => x % 2 != 0)
     .Distinct()
     .ToList();
-
-
-
+#endregion
+#region Task_2
 //  2.  Дана целочисленная последовательность. Извлечь из нее все
 //положительные двузначные числа, отсортировав их по возрастанию.
 
-static List<int> Task_2(List<int> items)
-{
-    return items
+static List<int> Task_2(List<int> items) => items
         .Where(x => x > 0 && x.ToString().Length == 2)
         .OrderBy(y => y)
         .ToList();
-}
-
+#endregion
+#region Task_3
 //  3.  Дана строковая последовательность. Строки последовательности содержат только
 //заглавные буквы латинского алфавита. Отсортировать последовательность
 //по возрастанию длин строк, а строки одинаковой длины – по убыванию.
 
+static List<string> Task_3(List<string> items) => items
+        .OrderBy(x => x.Length)
+        .ThenByDescending(x => x)
+        .ToList();
+#endregion
+#region Task_4
+//  4.  Дана последовательность непустых строк A. Получить последовательность символов,
+//каждый элемент которой является начальным символом соответствующей строки из A.
+//Порядок символов должен быть обратным по отношению к порядку элементов исходной последовательности.
+static List<char> Task_4(List<string> items) => items
+        .Select(x => x[0])
+        .Reverse()
+        .ToList();
+#endregion
+
+#region Task_5
+//  5.  Дана целочисленная последовательность. Обрабатывая только положительные числа,
+//получить последовательность их последних цифр и удалить в полученной последовательности
+//все вхождения одинаковых цифр, кроме первого. Порядок полученных цифр
+//должен соответствовать порядку исходных чисел.
+static List<int> Task_5(List<int> items)
+{
+    return items;
+}
+#endregion
 
 
-static void Print(List<int> items)
+
+static void Print<T>(List<T> items)
 {
     foreach (var item in items)
     {
