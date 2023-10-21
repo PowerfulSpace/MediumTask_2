1c1c3d2fe804bba5af1d5fcc4fa21d5827f6e551
Alexander <sany199574@gmail.com>
2023-10-21 06:52:25 +0300

    Добавлен тестовф пример, по правильныой десериализации

--- Test_3/Program.cs
+++ Test_3/Program.cs
@@ -1,24 +1,20 @@
-﻿
+﻿using System.Text.Json;
+
+Person tom = new Person("Tom", 37);
+string json = JsonSerializer.Serialize(tom);
+Console.WriteLine(json);
+Person? restoredPerson = JsonSerializer.Deserialize<Person>(json);
+Console.WriteLine(restoredPerson?.Name); // Tom
 
 
-
-
-
-string Classify(Point point) => point switch
+Console.ReadLine();
+class Person
 {
-    (0, 0) => "Origin",
-    (1, 0) => "positive X basis end",
-    (0, 1) => "positive Y basis end",
-    _ => "Just a point",
-};
-
-public readonly struct Point
-{
-    public int X { get; }
-    public int Y { get; }
-
-    public Point(int x, int y) => (X, Y) = (x, y);
-
-    public void Deconstruct(out int x, out int y) => (x, y) = (X, Y);
+    public string Name { get; }
+    public int Age { get; set; }
+    public Person(string name, int age)
+    {
+        Name = name;
+        Age = age;
+    }
 }
-
