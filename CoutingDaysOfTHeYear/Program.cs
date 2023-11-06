a4e3b5194f92096001ea290f66f4f4e6f18eda5e
Alexander <sany199574@gmail.com>
2023-11-06 08:00:27 +0300

    Добавлена задача, котороая считает количество дней в каждом месяце, и выдаёт их общую сумму

---
+++ CoutingDaysOfTHeYear/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+var dateTime = DateTime.DaysInMonth(2023, 12);
+int count = 0;
+
+List<int> list = new List<int>();
+
+for (int i = 1; i <= 12; i++)
+{
+    count = DateTime.DaysInMonth(2023, i);
+    list.Add(count);
+}
+Console.WriteLine();
+foreach (var item in list)
+{
+    Console.Write(item + " ");
+}
+Console.WriteLine();
+var sum = list.Sum(x => x);
+Console.WriteLine(sum);
+
+Console.ReadLine();
