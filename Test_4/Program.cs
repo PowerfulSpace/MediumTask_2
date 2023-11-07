8d799c5b105858f35ae5af28e1221447e350fc51
Alexander <sany199574@gmail.com>
2023-11-08 02:52:38 +0300

    Добавлена локазализация

--- Test_4/Program.cs
+++ Test_4/Program.cs
@@ -1,6 +1,23 @@
+using System.Globalization;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddLocalization();
+
+var localizationOptions = new RequestLocalizationOptions();
+var supportedCultures = new[]
+{
+                new CultureInfo("en-US"),
+                new CultureInfo("es-ES")
+};
+localizationOptions.SupportedCultures = supportedCultures;
+localizationOptions.SupportedUICultures = supportedCultures;
+localizationOptions.SetDefaultCulture("en-US");
+localizationOptions.ApplyCurrentCultureToResponseHeaders = true;
+
+
 
 var app = builder.Build();
 
@@ -12,6 +29,8 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
+
+app.UseRequestLocalization(localizationOptions);
 
 app.UseRouting();
 
